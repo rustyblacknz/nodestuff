8dabeec7cd74784ff3ee2945d37bccdd201c7eaa
RustyNZ <123700654+rustyblacknz@users.noreply.github.com>
2023-02-03 09:39:29 +1300

    Add files via upload

---
+++ startarb.sh
@@ -0,0 +1,1 @@
+./nitro --l1.url ws://127.0.0.1:8550 --l2.chain-id=42161 --http.api net,web3,eth,debug --http.addr 0.0.0.0 --http.port 8552 --ws.api net,web3,eth,debug --ws.addr 0.0.0.0 --ws.port 8553 --init.url="https://snapshot.arbitrum.io/mainnet/nitro.tar"
